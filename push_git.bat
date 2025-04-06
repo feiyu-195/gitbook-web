3abd33398f90f553322c1dd198090e237ec300ce
feiyu195 <feiyunote@qq.com>
2025-04-06 16:17:03 +0800

    Feiyu

--- push_git.bat
+++ push_git.bat
@@ -12,31 +12,37 @@ set log_file=%~dp0..\push_log.txt
     echo 更改工作目录...
     cd /d "%~dp0"
     echo.
-
+) > "%log_file%" 2>&1
+(
     echo 开始构建网页...
     call gitbook build
     echo.
-
+) >> "%log_file%" 2>&1
+(
     echo 添加所有文件至 Git repository...
     git add .
     echo.
-
+) >> "%log_file%" 2>&1
+(
     echo 提交Commit...
     git commit -m "Feiyu"
     echo.
-
+) >> "%log_file%" 2>&1
+(
     echo 上传文件至父仓库 master 主分支...
     git push origin master
     echo.
-
+) >> "%log_file%" 2>&1
+(
     echo 分离 _book 文件夹为新分支web...
     git subtree split -P _book -b web
     echo.
-
+) >> "%log_file%" 2>&1
+(
     echo 上传 _book 文件夹至子仓库 master 分支...
     git push web-origin web:master
     echo.
-) > "%log_file%" 2>&1
+) >> "%log_file%" 2>&1
 
 :: 退出脚本
 exit
