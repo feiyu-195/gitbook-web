9d0b54d193c9b56e811da6a15ce252612cc4a6e3
feiyu195 <feiyunote@qq.com>
2025-04-06 18:10:58 +0800

    feiyu commit

--- gitbook/gitbook-plugin-umami/plugin.js
+++ gitbook/gitbook-plugin-umami/plugin.js
@@ -2,10 +2,10 @@ require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         var cfg = config['umami'];
 
-        var site = document.createElement("script");
-        site.defer = true;
-        site.src = cfg.url;
-        site.setAttribute("data-website-id", cfg.website-id);
+        var script = document.createElement("script");
+        script.defer = true;
+        script.src = cfg.url;
+        script.setAttribute("data-website-id", cfg.website-id);
 
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(script, s);
